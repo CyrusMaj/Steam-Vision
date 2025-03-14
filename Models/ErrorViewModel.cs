3dd9f9206adf2317edb3638a18226534f93dbf95
Cyrus Majidy <cyrus_maj@yahoo.com>
2025-03-13 22:12:14 -0400

    Initial commit, new ASP.NET Core MVC project created + SteamApiService.cs added.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SteamVision.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Services/SteamApiService.cs
@@ -0,0 +1,28 @@
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class SteamApiService
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _steamApiKey = "YOUR_STEAM_API_KEY";
+
+    public SteamApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string> GetGameListAsync()
+    {
+        string url = $"https://api.steampowered.com/ISteamApps/GetAppList/v2/";
+        var response = await _httpClient.GetStringAsync(url);
+        return response;
+    }
+
+    public async Task<string> GetGameDetailsAsync(string appId)
+    {
+        string url = $"https://store.steampowered.com/api/appdetails?appids={appId}";
+        var response = await _httpClient.GetStringAsync(url);
+        return response;
+    }
+}
